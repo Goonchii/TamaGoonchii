d065e49c68d7e16d28f0f12d47f61f1c09fae325
Goonchii <trashal666@gmail.com>
2026-04-11 19:29:23 +0100

    A better dragging window
    https://forum.godotengine.org/t/button-to-drag-a-window/127332/2

---
+++ scripts/tama_goonchii.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var offset: Vector2 = Vector2.ZERO
+var dragging: bool = false
+
+func _ready() -> void:
+	get_window().mouse_passthrough = false
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("LClick"):
+		dragging = true
+		offset = get_viewport().get_mouse_position()
+	if Input.is_action_just_released("LClick"):
+		dragging = false
+		offset = Vector2.ZERO
+	if dragging:
+		var current_mouse_pos: Vector2 = DisplayServer.mouse_get_position()
+		var window_pos: Vector2 = current_mouse_pos - offset
+		DisplayServer.window_set_position(window_pos)
+	
+	#get_window().mouse_passthrough_polygon = $Polygon2D.polygon
+	#if Input.is_action_just_pressed("LClick"):
+		#print("clicked")
