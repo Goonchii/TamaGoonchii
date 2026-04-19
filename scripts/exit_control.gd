f844d61cabb68a6fac18161f69c5cdab2632c0c5
Goonchii <trashal666@gmail.com>
2026-04-19 21:15:55 +0100

    Reorganised + implemented exit menu

---
+++ scripts/exit_control.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+@onready var main: Node2D = get_node("/root/TamaGoonchii/Screen/Icons/SelectControl")
+@onready var exit_layer: CanvasGroup = get_node("/root/TamaGoonchii/Screen/Exit")
+
+@onready var arrow_label: Label = get_node("/root/TamaGoonchii/Screen/Exit/Arrow")
+@onready var yes_label: Label = get_node("/root/TamaGoonchii/Screen/Exit/Yes")
+@onready var no_label: Label = get_node("/root/TamaGoonchii/Screen/Exit/No")
+
+var exit_active: bool = false
+var exit_index: int = 0
+var exit_items: Array[Label] = []
+
+# TODO: Add H/Vboxes for menu
+
+func _on_a_button_up() -> void:
+	if not Global.a_active: return
+	if exit_active:
+		exit_index = 1 - exit_index
+		move_arrow()
+
+func _on_b_button_up() -> void:
+	if not Global.b_active: return
+	if exit_active:
+		print("Selected: ", exit_index)
+		if exit_index == 0:
+			get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+			get_tree().quit()
+		else: if exit_index == 1:
+			deactivate_exit()
+			main.activate_main()
+
+func exit_menu() -> void:
+	exit_layer.show()
+	exit_active = true
+	Global.a_active = true
+	Global.b_active = true
+	Global.c_active = false
+	exit_index = 0
+	exit_items = [yes_label, no_label]
+func deactivate_exit() -> void:
+	exit_layer.hide()
+	exit_active = false
+	Global.a_active = false
+	Global.b_active = false
+	Global.c_active = false
+
+func move_arrow() -> void:
+	var target := exit_items[exit_index] as Control
+	arrow_label.global_position.y = target.global_position.y + target.size.y / 2 - arrow_label.size.y / 2
--- scripts/select_control.gd
+++ scripts/select_control.gd
@@ -1,67 +1,81 @@
 extends Node2D
 
-@onready var icons_layer: CanvasLayer = get_node("/root/TamaGoonchii/Icons")
+@onready var icons_layer: CanvasGroup = get_node("/root/TamaGoonchii/Screen/Icons")
+
+@onready var exit_control: Node2D = get_node("/root/TamaGoonchii/Screen/Exit/ExitControl")
 
 var main_is_active: bool = false
-var current_index: int = 0
+
+var select_index: int = 0
 var icons: Array = []
 
 func _ready() -> void:
 	# TODO: Move this later
+	# TODO: Add H+V boxes for icons/screen
 	activate_main()
+	Global.hide_layers()
 	
 	icons = get_tree().get_nodes_in_group("Icons")
 	for icon in icons:
 		icon.modulate = Color(0.0, 0.0, 0.0, 0.471)
-	icons[current_index].modulate = Color(1.0, 1.0, 1.0, 0.784)
+	icons[select_index].modulate = Color(1.0, 1.0, 1.0, 0.784)
 
 func _on_a_button_up() -> void:
-	if not main_is_active: return
-	unhighlight_icon(icons[current_index])
-	current_index += 1 % icons.size()
-	if current_index > 7:
-		current_index = 0
-	highlight_icon(icons[current_index])
+	if not Global.a_active: return
+	if main_is_active:
+		unhighlight_icon(icons[select_index])
+		select_index = (select_index + 1) % icons.size()
+		highlight_icon(icons[select_index])
 
 func _on_b_button_up() -> void:
-	if not main_is_active: return
-	main_is_active = false
-	match current_index:
-		0:
-			icons_layer.hide()
-			print("Feed")
-		1:
-			icons_layer.hide()
-			print("Play")
-		2:
-			icons_layer.hide()
-			print("Clean")
-		3:
-			icons_layer.hide()
-			print("Sleep")
-		4:
-			icons_layer.hide()
-			print("Shovel")
-		5:
-			icons_layer.hide()
-			print("Meds")
-		6:
-			icons_layer.hide()
-			print("Status")
-		7:
-			icons_layer.hide()
-			print("Menu")
+	if not Global.b_active: return
+	if main_is_active:
+		match select_index:
+			0:
+				deactivate_main()
+				print("Feed") # TODO: Feed func goes here + etc V
+			1:
+				deactivate_main()
+				print("Play")
+			2:
+				deactivate_main()
+				print("Clean")
+			3:
+				deactivate_main()
+				print("Sleep")
+			4:
+				deactivate_main()
+				print("Shovel")
+			5:
+				deactivate_main()
+				print("Meds")
+			6:
+				deactivate_main()
+				print("Status")
+			7:
+				deactivate_main()
+				print("Menu")
 
 func _on_c_button_up() -> void:
-	if not main_is_active: return
+	if not Global.c_active: return
+	if main_is_active:
+		deactivate_main()
+		exit_control.exit_menu()
 
 func activate_main() -> void:
 	icons_layer.show()
 	main_is_active = true
-
+	Global.a_active = true
+	Global.b_active = true
+	Global.c_active = true
+func deactivate_main() -> void:
+	icons_layer.hide()
+	main_is_active = false
+	Global.a_active = false
+	Global.b_active = false
+	Global.c_active = false
 
 func unhighlight_icon(icon: Node) -> void:
 	icon.modulate = Color(1.0, 1.0, 1.0, 0.471)
-
 func highlight_icon(icon: Node) -> void:
 	icon.modulate = Color(1.0, 1.0, 1.0, 0.784)
